76b4f974f15c94e663bff6e59810ad670f43c28a
williarde <131699279+williardet@users.noreply.github.com>
2026-01-21 21:07:32 +0100

    Change uvicorn app reference from 'app' to 'main'

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def search_pokemon(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 @app.get("/")
 def read_root():
